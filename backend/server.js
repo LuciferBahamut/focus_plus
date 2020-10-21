d563d702e045436ae4c64ceb2b77e94c8ac31548
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-10-21 15:39:58 +0200

    [UPD] del route / save / exec script and move in file route users

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 //const moogoose = require('mongoose'); A installer si besoin
-const multer = require('multer');
-const fs = require('fs');
-const exec = require('child_process').exec;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,34 +8,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//const userRouter = require('./routes/users');
+const userRouter = require('./routes/users');
 
-var storage = multer.diskStorage({
-    destination : function(req, file, callback) {
-        fs.mkdir('./upload', function(err) {
-            if(err) {
-                console.log(err.stack);
-            } else {
-                callback(null, './upload');
-            }
-        })
-    },
-    filename: function(req, file, callback) {
-        callback(null, 'image.png');
-    }
-});
-
-app.post('/users/upload', function(req, res) {
-    var upload = multer({storage : storage}).single('image');
-    upload(req, res, function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded")
-    });
-    exec('sh test.sh /myDir');
-});
-
+app.use("/users", userRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
