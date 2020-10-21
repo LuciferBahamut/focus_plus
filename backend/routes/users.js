9224b510abe56df83123ab789045a6ac83459a3a
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-10-21 15:39:26 +0200

    [UPD] route / save / exec scritp in file route for users upload

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,32 @@
 const router = require('express').Router();
 const multer = require('multer');
+const fs = require('fs');
+const exec = require('child_process').exec;
 
-router.post('/upload', multer.any("image"), function(req, res) {
-    console.log(req.body);
-    const files = req.body;
-    console.log(files);
-    console.log("Uploaded file: ", req.file);
-    //res.redirect("/");   
+var storage = multer.diskStorage({
+    destination : function(req, file, callback) {
+        fs.mkdir('./upload', function(err) {
+            if(err) {
+                console.log(err.stack);
+            } else {
+                callback(null, './upload');
+            }
+        })
+    },
+    filename: function(req, file, callback) {
+        callback(null, 'image.png');
+    }
+});
+
+router.post('/upload', function(req, res) {
+    var upload = multer({storage : storage}).single('image');
+    upload(req, res, function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded")
+    });
+    exec('sh test.sh /myDir');
 });
 
 module.exports = router;
