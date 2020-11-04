b5a3a8f99cdc478e1642e0b212d56680625ee430
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-11-04 16:41:40 +0100

    [ADD] the file to connect with database

---
+++ server/db/db.js
@@ -0,0 +1,16 @@
+// This file is here if you use a Database for this project (MongoDB more exactly)
+
+require('dotenv').config();
+const mongoose = require('mongoose'); //execute npm install mongoose because it's not installed !
+
+//const uri = process.env.ATLAS_URI;
+const uri = 'mongodb+srv://User_test:User_test@cluster0.huyln.mongodb.net/Focus+?retryWrites=true&w=majority';
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+const connection = mongoose.connection; // Connection avec la base de donnée MongoDB
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully"); //Affiche dans la console le message si dessus si la connection s'effectue
+});
