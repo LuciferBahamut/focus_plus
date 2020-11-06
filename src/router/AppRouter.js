d4e006a9e608ffef4b9f9a0c1e29ba2e142413c7
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-11-06 10:20:46 +0100

    [UPD] add the news link with the news pages

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from '../components/App';
 import Header from '../components/Header';
-import FileList from "../components/FileList"
+import Video from '../components/Video';
+import Forum from '../components/Forum';
+import Home from '../components/Home';
+import ListFile from '../components/FileList';
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -10,8 +13,11 @@ const AppRouter = () => (
       <Header />
       <div className="main-content">
         <Switch>
-          <Route component={App} path="/" exact={true} />
-          <Route component={FileList} path='/list' />
+          <Route component={Home} path="/" exact={true} />
+          <Route component={App} path="/focus+" exact={true} />
+          <Route component={Video} path="/video" exact={true} />
+          <Route component={Forum} path="/forum" exact={true} />
+          <Route component={ListFile} path="/list" exact={true} />
         </Switch>
       </div>
     </div>
