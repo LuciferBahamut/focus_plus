35edc5250a4d2cdbf31456a04a1211d682a464e0
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-11-06 10:19:50 +0100

    [ADD] video page

---
+++ src/components/Video.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+const Video = () => {
+
+return (
+    <React.Fragment>
+        <Row>
+          <Col>
+            Bienvenue sur la partie vidéo
+          </Col>
+        </Row>
+    </React.Fragment>
+    );
+};
+
+export default Video;
