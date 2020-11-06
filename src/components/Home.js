316eab58826ba9e515e2d7a010bcf0b6124a6970
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-11-06 10:19:12 +0100

    [ADD] home page

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+const Home = () => {
+
+return (
+    <React.Fragment>
+        <Row>
+          <Col>
+            Bienvenue sur Focus +
+          </Col>
+        </Row>
+    </React.Fragment>
+    );
+};
+
+export default Home;
