151175482519efbae72f35daa9ee1c72ac6c7278
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-11-06 10:19:29 +0100

    [ADD] forum page

---
+++ src/components/Forum.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+const Forum = () => {
+
+return (
+    <React.Fragment>
+        <Row>
+          <Col>
+            Bienvenue sur la partie Forum
+          </Col>
+        </Row>
+    </React.Fragment>
+    );
+};
+
+export default Forum;
