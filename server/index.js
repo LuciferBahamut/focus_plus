7f451e24f1964d3a3ad1854129bfa7edb8197806
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-11-18 18:00:01 +0100

    [UPD] use new dependencies for routes post for user

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const fileRoute = require('./routes/file');
 const userRoute = require('./routes/user');
@@ -8,6 +9,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(fileRoute);
 app.use(userRoute);
 
