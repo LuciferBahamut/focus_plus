263162fbea8875426db8f2d0df071d7cbd853485
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-11-19 17:38:10 +0100

    [UPD] add getAllMessage

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { API_URL } from '../utils/constants';
 
 const Forum = () => {
+
+  const [username, setUsername] = useState('');
+  const [userMessage, setUserMessage] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  useEffect(() => {
+    const getMessage = async () => {
+      try {
+        const { data } = await axios.get(`${API_URL}/getAllMessage`);
+        setErrorMsg('');
+        setUserMessage(data);
+      } catch (error) {
+        error.response && setErrorMsg(error.response.data);
+      }
+    };
+
+    getMessage();
+  }, []);
 
 return (
     <React.Fragment>
