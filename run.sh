044114ea16a84f8d28db8a8709302ef36bd53b50
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-11-27 11:57:27 +0100

    [UPD] add if the folder node_module isn't exist

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/bash
 
 cd server
+if [ -d node_modules ] then
+    npm start &
+else
+npm install
 npm start &
+fi
 
 cd ..
+if [ -d node_modules ] then
+    npm start
+else
+npm install
 npm start
+fi
