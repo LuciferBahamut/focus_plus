f520b9ae4590a9dc34997c6102057b8e6f2481ed
Jamal Kansao <jamal.kansao2@gmail.com>
2020-12-09 20:49:31 +0100

    [Mod] changement de style de la page d'accueil, du header et du footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,33 +2,37 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Focus_plus from '../image/logo_focus+.png';
 import Logo from '../image/logo_ministère.png';
+import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
   return (
+    <Container fluid style={{paddingLeft: '0px', paddingRight: '0px'}}>
     <div className="header">
-      <img src={Logo}
-      alt="ministère logo"
-      title="Logo du ministère"></img>
-      <img src={Focus_plus}
-      alt="Focus+ logo"
-      height="200"
-      title="Focus + logo"></img>
-      <nav>
-        <NavLink activeClassName="active" to="/" exact={true}>
-          Accueil
-        </NavLink>
-        <NavLink activeClassName="active" to="/focus+">
-          Focus+
-        </NavLink>
-        <NavLink activeClassName="active" to="/video">
-          Video tuto
-        </NavLink>
-        <NavLink activeClassName="active" to="/forum">
-          Forum
-        </NavLink>
-      </nav>
+      <Row noGutters>
+        <Col md={4}>
+          <img src={Logo}
+          alt="ministère logo"
+          title="Logo du ministère"></img>
+        </Col>
+        <Col md={{ span: 4, offset: 4 }}>
+          <img src={Focus_plus}
+          alt="Focus+ logo"
+          height="100"
+          title="Focus + logo"></img>
+        </Col>
+      </Row>
       <p>Ceci n'est qu'un prototype</p>
+      <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="/">Accueil</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/focus+">Conversion</Nav.Link>
+        <Nav.Link href="/video">Tutoriels</Nav.Link>
+        <Nav.Link href="/forum">Forum d'entraide</Nav.Link>
+        <Nav.Link href="/">À propos</Nav.Link>
+      </Nav>
+    </Navbar>
     </div>
+    </Container>
   );
 };
 
