0046d6be3b3d3b6d59e1755087a2c6bd11ae208d
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-12-11 16:47:53 +0100

    [UPD] change the connection at mongodb server

--- server/db/db.js
+++ server/db/db.js
@@ -2,13 +2,16 @@
 
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://User_test:User_test@cluster0.huyln.mongodb.net/Focus+?retryWrites=true&w=majority';
+//const uri = 'mongodb+srv://User_test:User_test@cluster0.huyln.mongodb.net/Focus+?retryWrites=true&w=majority';
+const uri = "mongodb://localhost:27017/focus";
+
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
 });
-const connection = mongoose.connection; // Connection avec la base de donnée MongoDB
-connection.once('open', () => {
+const db = mongoose.connection; // Connection avec la base de donnée MongoDB
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
     console.log("MongoDB database connection established successfully"); //Affiche dans la console le message si dessus si la connection s'effectue
 });
