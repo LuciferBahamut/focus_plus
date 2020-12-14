3492f4ec00daf214627c38608b4eaf39ed2773ba
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-12-14 11:07:26 +0100

    [UPD] script auto run

--- Windows/run.bat
+++ Windows/run.bat
@@ -1,24 +1,14 @@
-if exist "%cd%\node_modules" (
-    start npm start
-) else (
+if not exist "%cd%\node_modules" (
     npm install
-    start npm start
 )
-pause
 cd .\server
-if exist "%cd%\files" (
-    if exist "%cd%\node_modules" (
-    start npm start
-    ) else (
+if not exist "%cd%\node_modules" (
     npm install
-    start npm start
-    )
-) else (
-    mkdir files
-    if exist "%cd%\node_modules" (
-    start npm start
-    ) else (
-    npm install
-    start npm start
-    )
 )
+if not exist "%cd%\files" (
+    mkdir files
+)
+
+start npm start
+cd ..
+start npm start
