8bff11b5fd41381830cbd31d1728181b2d8bbfff
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-12-14 15:12:22 +0100

    [UPD/ADD] split the run into two run one for front and oen for back

--- Windows/run.bat
+++ Windows/run.bat
@@ -1,20 +1,7 @@
+start runbis.bat
 if not exist "%cd%\node_modules" (
     npm install
     start npm start
 ) else (
-    start npm start
-)
-
-cd .\server
-if not exist "%cd%\node_modules" (
-    npm install
-    if not exist "%cd%\files" (
-    mkdir files
-    )
-    start npm start
-) else (
-    if not exist "%cd%\files" (
-    mkdir files
-    )
     start npm start
 )
