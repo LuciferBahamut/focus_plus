52ba49419f890e4690826115a49ce43ba141d23a
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-12-14 14:30:22 +0100

    [UPD] test the new auto run

--- Windows/run.bat
+++ Windows/run.bat
@@ -1,9 +1,9 @@
 if not exist "%cd%\node_modules" (
-    npm install
+    start npm install
 )
 cd .\server
 if not exist "%cd%\node_modules" (
-    npm install
+    start npm install
 )
 if not exist "%cd%\files" (
     mkdir files
